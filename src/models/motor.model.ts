ad3e5086ac6206600481fe1aa11f86a61c30ead1
QarthO <qartho@gmail.com>
2022-11-26 10:01:57 -0500

    smart motor positions 1-100 equation

--- src/models/motor.model.ts
+++ src/models/motor.model.ts
@@ -3,51 +3,47 @@ import { Motor } from '@/interfaces/motor.interface';
 // password: password
 const motorModel: Motor[] = [
   // FEET
-  { id: 0, name: 'left-foot', position: 0, min: 1, max: 254 },
-  { id: 1, name: 'right-foot', position: 0, min: 0, max: 255 },
-  { id: 2, name: 'left-heel', position: 0, min: 0, max: 255 },
-  { id: 3, name: 'right-heel', position: 0, min: 0, max: 255 },
+  { id: 0, name: 'left-foot', description: 'foot tilt in/out', position: 0, min: 115, max: 140, default: 127, inverted: true },
+  { id: 1, name: 'right-foot', description: 'foot tilt in/out', position: 0, min: 110, max: 140, default: 127, inverted: false },
+  { id: 2, name: 'left-heel', description: 'foot up/down', position: 0, min: 77, max: 175, default: 127, inverted: true },
+  { id: 3, name: 'right-heel', description: 'foot up/down', position: 0, min: 78, max: 175, default: 127, inverted: false },
 
   // KNEE
-  { id: 4, name: 'left-knee', position: 0, min: 0, max: 255 },
-  { id: 5, name: 'right-knee', position: 0, min: 0, max: 255 },
+  { id: 4, name: 'left-knee', description: 'shin front/back', position: 0, min: 77, max: 204, default: 204, inverted: true },
+  { id: 5, name: 'right-knee', description: 'shin fron/back', position: 0, min: 50, max: 180, default: 50, inverted: false },
 
   // UPPER LEG
-  { id: 6, name: 'left-front-upper-leg', position: 0, min: 0, max: 255 },
-  { id: 7, name: 'right-front-upper-leg', position: 0, min: 0, max: 255 },
-  { id: 8, name: 'left-back-upper-leg', position: 0, min: 0, max: 255 },
-  { id: 9, name: 'right-back-upper-leg', position: 0, min: 0, max: 255 },
+  { id: 6, name: 'left-front-upper-leg', description: '', position: 0, min: 0, max: 255, default: 127, inverted: true },
+  { id: 7, name: 'right-front-upper-leg', description: '', position: 0, min: 0, max: 255, default: 127, inverted: false },
+  { id: 8, name: 'left-back-upper-leg', description: '', position: 0, min: 0, max: 255, default: 127, inverted: true },
+  { id: 9, name: 'right-back-upper-leg', description: '', position: 0, min: 0, max: 255, default: 127, inverted: false },
 
   // HIP
-  { id: 10, name: 'left-hip', position: 0, min: 0, max: 255 },
-  { id: 11, name: 'right-hip', position: 0, min: 0, max: 255 },
+  { id: 10, name: 'left-hip', description: '', position: 0, min: 0, max: 255, default: 127, inverted: true },
+  { id: 11, name: 'right-hip', description: '', position: 0, min: 0, max: 255, default: 127, inverted: false },
 
   // shoulder
-  { id: 12, name: 'left-upper-shoulder', position: 0, min: 0, max: 255 },
-  { id: 13, name: 'right-upper-shoulder', position: 0, min: 0, max: 255 },
-  { id: 14, name: 'left-lower-shoulder', position: 0, min: 0, max: 255 },
-  { id: 15, name: 'right-lower-shoulder', position: 0, min: 0, max: 255 },
+  { id: 12, name: 'left-upper-shoulder', description: '', position: 0, min: 10, max: 254, default: 180, inverted: true },
+  { id: 13, name: 'right-upper-shoulder', description: '', position: 0, min: 1, max: 254, default: 180, inverted: false },
+  { id: 14, name: 'left-lower-shoulder', description: '', position: 0, min: 1, max: 120, default: 115, inverted: true },
+  { id: 15, name: 'right-lower-shoulder', description: '', position: 0, min: 130, max: 254, default: 138, inverted: false },
 
   // bicep
-  { id: 16, name: 'left-bicep', position: 0, min: 0, max: 255 },
-  { id: 17, name: 'right-bicep', position: 0, min: 0, max: 255 },
+  { id: 16, name: 'left-bicep', description: 'twist in', position: 0, min: 1, max: 254, default: 127, inverted: true },
+  { id: 17, name: 'right-arm', description: 'twist in', position: 0, min: 1, max: 254, default: 127, inverted: false },
 
   // elbow
-  { id: 18, name: 'left-eblow', position: 0, min: 0, max: 255 },
-  { id: 19, name: 'right-elbow', position: 0, min: 0, max: 255 },
-
-  // hands
-  { id: 20, name: 'left-hand', position: 0, min: 0, max: 255 },
-  { id: 21, name: 'right-hand', position: 0, min: 0, max: 255 },
+  { id: 18, name: 'left-eblow', description: 'max is up', position: 0, min: 48, max: 190, default: 48, inverted: true },
+  { id: 19, name: 'right-elbow', description: 'min is up', position: 0, min: 65, max: 210, default: 210, inverted: false },
 
   // waist
-  { id: 22, name: 'waist', position: 0, min: 0, max: 255 },
+  { id: 22, name: 'hip', description: 'rotate left/right', position: 0, min: 90, max: 170, default: 127, inverted: false },
 
   // neck
-  { id: 23, name: 'neck', position: 0, min: 0, max: 255 },
+  { id: 23, name: 'neck', description: 'look left/right', position: 0, min: 1, max: 254, default: 127, inverted: false },
 
-  // head
-  { id: 24, name: 'head', position: 0, min: 0, max: 255 },
+  // head 
+  { id: 24, name: 'head', description: 'look up/down', position: 0, min: 70, max: 160, default: 127, inverted: false },
 ];
 
 export default motorModel;
