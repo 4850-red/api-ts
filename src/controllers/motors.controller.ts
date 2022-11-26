666f044ebe2048e4d07f473317043f3e985259a5
QarthO <qartho@gmail.com>
2022-11-26 10:34:33 -0500

    flipped invert for smart position

--- src/controllers/motors.controller.ts
+++ src/controllers/motors.controller.ts
@@ -77,11 +77,12 @@ class MotorsController {
   private convertMotorPosition = (motor: Motor, newPosition: string) => {
     let smartPosition: number = Number(newPosition)
     let div = motor.inverted ? motor.max : motor.min
-    smartPosition = motor.inverted ? smartPosition : 100-smartPosition
+    smartPosition = motor.inverted ? 100-smartPosition : smartPosition
     let rawPosition = smartPosition/div * 254
 
     console.table({
       newPosition: newPosition,
+      inverted: motor.inverted,
       div: div,
       smartPosition: smartPosition,
       rawPosition: rawPosition
